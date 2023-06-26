da8ad95ed505b8181a83c0e9ad546fc70bab9f81
Jack <jack.dowww@gmail.com>
2023-06-26 22:35:58 +1000

    Change meta description and add keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,37 @@ import { cx } from "~/lib/utils";
 
 import "../styles/globals.css";
 
+import { type Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Toaster } from "~/components/ui/toaster";
 
 const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Dogworx Hydrotherapy | The care your dog would choose",
 	description:
-		"Dogworx Hydrotherapy provides hydrotherapy services to dogs in need of physical rehabilitation, fitness, and overall wellness.",
+		"Dogworx Hydrotherapy offers state-of-the-art hydrotherapy services on the Gold Coast. Our underwater treadmill can help dogs in a variety of circumstances from arthritis to post-surgery recovery to fitness and conditioning. Contact us today to learn more!",
+	keywords: [
+		"hydrotherapy",
+		"underwater treadmill",
+		"Gold Coast",
+		"Upper Coomera",
+		"arthritis",
+		"post-surgery recovery",
+		"recovery",
+		"fitness",
+		"conditioning",
+		"dog",
+		"dogs",
+		"pet",
+		"canine",
+		"dogworx",
+		"dogworx hydrotherapy",
+		"rehabilitation",
+		"hip dysplasia",
+		"cruciate ligament",
+	],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
